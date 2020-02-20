17d2c1ac9c00b3bb6dabd8034260704041cb7685
faizan352 <faizanm352@gmail.com>
2020-02-20 16:04:04 +0530

    latest changes

--- new/views.py
+++ new/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here testing edit
 
 def home(request):
     return HttpResponse("Hello faizan")
